022199ef30c9fc9d65ca60c69ef771478ecac9cc
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2026-04-15 21:23:07 +0200

    Changes

--- docker/build_system_test.sh
+++ docker/build_system_test.sh
@@ -14,23 +14,38 @@ else
     echo "Image '$IMAGE_NAME' already exists, skipping build."
 fi
 
-echo "Starting container and building system-test target..."
+echo "Starting container and building targets..."
 
 docker run --rm \
     -v "$(pwd):/workspace" \
     "$IMAGE_NAME" \
     bash -c "
         set -e
+
+        echo '--- Building FreeType ---'
+        rm -rf /workspace/src/externals/freetype/build
+        cd /workspace/src/externals/freetype
+        cmake -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF \
+              -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+              -DFT_DISABLE_BZIP2=ON -DFT_DISABLE_BROTLI=ON \
+              -DFT_DISABLE_ZLIB=ON -DFT_DISABLE_PNG=ON \
+              -Wno-dev
+        cmake --build build -j\$(nproc)
+
         echo '--- Configuring ---'
         cmake -S /workspace/src \
               -B $BUILD_DIR \
               -DCMAKE_BUILD_TYPE=Debug \
               -DCMAKE_PREFIX_PATH='/usr/lib/x86_64-linux-gnu' \
-              -G Ninja
+              -G Ninja -Wno-dev
+
+        echo '--- Building duoplot ---'
+        cmake --build $BUILD_DIR --target duoplot -j\$(nproc)
+        file /workspace/src/build/duoplot
 
         echo '--- Building system-test ---'
         cmake --build $BUILD_DIR --target system-test -j\$(nproc)
+        file $BUILD_DIR/system_test/system-test
 
         echo '--- Build successful ---'
-        file /workspace/src/build/system-test
     "
